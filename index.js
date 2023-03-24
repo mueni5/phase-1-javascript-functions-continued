7c42c04bc071462212f55b9f6893109cc4833a53
+ <+>
2023-03-24 15:40:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return "This Saturday, I want to roller-skate!";
+    }
+    console.log(saturdayFun()); //"This Saturday, I want to bathe my dog!"
+console.log(saturdayFun("bathe my dog")); //"This saturday, I want to bathe my dog!"
+
+const mondayWork = function work(activity="go to the office") {
+    return "This Monday, I will go to the office.";
+    const mondayWork = ("work from home"); //"This monday, I will work from home."
+    }
+    
+    
+function wrapAdjective(flair = "*") {
+    return function(adjective = "special") {
+        return "You are ${flair}${adjective}${flair}!"; 
+    }
+    
+}
+
+const christine = wrapAdjective ()
+    
+
+
+
+      
+
+      
+    
+
